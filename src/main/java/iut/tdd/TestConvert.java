6bcd9af9b03062c511000686da555ff541125525
Camille Levoye <camille.levoye@orange.fr>
2015-03-30 16:53:51 +0200

    1

--- src/main/java/iut/tdd/TestConvert.java
+++ src/main/java/iut/tdd/TestConvert.java
@@ -6,15 +6,143 @@ import org.junit.Test;
 
 public class TestConvert {
 	@Test
-	public void test_num2text_zero () {
-		// Given
-		String input = "0";
-		String expected = "zéro";
+	public void test_num2text_zero() {
+
 		Convert myConvert = new Convert();
-		// When
-		String actual = myConvert.num2text(input);
-		// Then
-		Assert.assertEquals(expected, actual);
+		Assert.assertEquals("zéro", myConvert.num2text("0"));
+
+	}
+
+	@Test
+	public void test_num2text_un() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("un", myConvert.num2text("1"));
+	}
+
+	@Test
+	public void test_num2text_deux() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("deux", myConvert.num2text("2"));
+	}
+
+	@Test
+	public void test_num2text_trois() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("trois", myConvert.num2text("3"));
+	}
+
+	@Test
+	public void test_num2text_quatre() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("quatre", myConvert.num2text("4"));
+	}
+
+	@Test
+	public void test_num2text_cinq() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("cinq", myConvert.num2text("5"));
+	}
+
+	@Test
+	public void test_num2text_six() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("six", myConvert.num2text("6"));
+	}
+
+	@Test
+	public void test_num2text_sept() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("sept", myConvert.num2text("7"));
+	}
+
+	@Test
+	public void test_num2text_huit() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("huit", myConvert.num2text("8"));
+	}
+
+	@Test
+	public void test_num2text_neuf() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("neuf", myConvert.num2text("9"));
+	}
+
+	@Test
+	public void test_num2text_dix() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("dix", myConvert.num2text("10"));
+	}
+
+	@Test
+	public void test_num2text_onze() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("onze", myConvert.num2text("11"));
+	}
+
+	@Test
+	public void test_num2text_douze() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("douze", myConvert.num2text("12"));
+	}
+
+	@Test
+	public void test_num2text_treize() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("treize", myConvert.num2text("13"));
+	}
+
+	@Test
+	public void test_num2text_quatorze() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("quatorze", myConvert.num2text("14"));
+	}
+
+	@Test
+	public void test_num2text_quinze() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("quinze", myConvert.num2text("15"));
+	}
+
+	@Test
+	public void test_num2text_seize() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("seize", myConvert.num2text("16"));
+	}
+	
+	@Test
+	public void test_num2text_17() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("dix-sept", myConvert.num2text("17"));
+	}
+	
+	@Test
+	public void test_num2text_vingt() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("vingt", myConvert.num2text("20"));
+	}
+	
+	@Test
+	public void test_num2text_vingt_et_un() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("vingt et un", myConvert.num2text("21"));
+	}
+	
+	@Test
+	public void test_num2text_22() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("vingt-deux", myConvert.num2text("22"));
+	}
+	
+	@Test
+	public void test_num2text_31() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("trente et un", myConvert.num2text("31"));
+	}
+	
+	@Test
+	public void test_num2text_81() {
+		Convert myConvert = new Convert();
+		Assert.assertEquals("quatre-vingt-un", myConvert.num2text("81"));
 	}
 	
 }
--- src/test/java/iut/tdd/Convert.java
+++ src/test/java/iut/tdd/Convert.java
@@ -1,9 +1,70 @@
 package iut.tdd;
 
+import java.util.HashMap;
+
 public class Convert {
+
 	public static String num2text(String input) {
-		return null;
+		String result = "";
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("0", "zéro");
+		map.put("1", "un");
+		map.put("2", "deux");
+		map.put("3", "trois");
+		map.put("4", "quatre");
+		map.put("5", "cinq");
+		map.put("6", "six");
+		map.put("7", "sept");
+		map.put("8", "huit");
+		map.put("9", "neuf");
+		map.put("10", "dix");
+		map.put("11", "onze");
+		map.put("12", "douze");
+		map.put("13", "treize");
+		map.put("14", "quatorze");
+		map.put("15", "quinze");
+		map.put("16", "seize");
+		map.put("20", "vingt");
+		map.put("30", "trente");
+		map.put("40", "quarente");
+		map.put("50", "cinquante");
+		map.put("60", "soixante");
+		map.put("70", "soixante-dix");
+		map.put("80", "quatre-vingt");
+		map.put("90", "quatre-vingt-dix");
+
+		if (Integer.parseInt(input) <= 16) {
+			result = map.get(input);
+		} else if (input.charAt(1) == '0') {
+			result = map.get(input);
+		} else if (input.charAt(1) == '1') {
+			if (2 <= Integer.parseInt(input.charAt(0)+"") || Integer.parseInt(input.charAt(0)+"") <= 6) {
+				String tmp = getDizaine(Integer.parseInt(input));
+				result = map.get(tmp) + "-et-un";
+			}
+			else if(Integer.parseInt(input.charAt(0)+"")==7){
+				String tmp = input.charAt(0) + "0";
+				result = map.get(tmp) + "-et-onze";
+			}
+			else{
+				
+			}
+		} else {
+			String dizaine = input.charAt(0) + "0";
+			String unite = input.charAt(1) + "";
+			result = map.get(dizaine) + "-" + map.get(unite);
+		}
+
+		return result;
+
 	}
+	
+	public static String getDizaine(int input){
+		int dizaine = input/10;
+		return null;
+		
+	}
+
 	public static String text2num(String input) {
 		return null;
 	}
